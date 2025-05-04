17eff1a7a26ad85c2167f0e75269bda80682b69a
Rain <rainamy0825@gmail.com>
2025-05-04 11:19:11 +0800

    新增 app.py：Flask 應用進入點

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, request, abort
+from dotenv import load_dotenv
+
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from bot.handler import reply_echo_message
+
+load_dotenv()
+
+app = Flask(__name__)
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    reply_echo_message(event)
+
+if __name__ == "__main__":
+    app.run(port=8000)
